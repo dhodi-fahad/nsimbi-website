b70ea92729dbea9bb6520fb98670b9bd578077fa
dhodi-fahad <dhodifahad@gmail.com>
2024-01-09 15:51:47 +0300

    integrated firebase hosting

--- app/page.tsx
+++ app/page.tsx
@@ -7,27 +7,27 @@ import Order from "@/components/order";
 
 export default function Home() {
 
-  return (
-    <div className={styles.page}>
+    return (
+        <div className={styles.page}>
 
-        <div className={styles.hero}>
-            <Order/>
-            <div>
-                <Image className={styles.image} src={"/Hero.svg"} width={800} height={300} priority alt="Nsimbi Shopping Hero Image"/>
+            <div className={styles.hero}>
+                <Order/>
+                <div>
+                    <Image className={styles.image} src={"/Hero.svg"} width={800} height={300} priority alt="Nsimbi Shopping Hero Image"/>
+                </div>
             </div>
-        </div>
 
-        <h1>How it works.</h1>
+            <h1>How it works.</h1>
             <div className={styles.card}>
-              <h3>
-                Step 2
-              </h3>
-              <h2>
-                Pick Your Market
-              </h2>
-              <p>Select the specific market for your shopping.</p>
+                <h3>
+                    Step 2
+                </h3>
+                <h2>
+                    Pick Your Market
+                </h2>
+                <p>Select the specific market for your shopping.</p>
             </div>
 
-    </div>
-  )
+        </div>
+    )
 }
